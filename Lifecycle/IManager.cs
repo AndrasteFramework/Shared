7a5d7d377cfb5e6e37409576d485c3e1ce0b29e7
MeFisto94 <MeFisto94@users.noreply.github.com>
2021-05-26 22:50:41 +0200

    Initial commit of the Andraste Shared Project

---
+++ Lifecycle/IManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Andraste.Shared.Lifecycle
+{
+    /// <summary>
+    /// A manager is a lifecycle unit within Andraste.
+    /// Every service should be a manager, so it can properly be loaded,
+    /// unloaded, enabled and disabled.
+    /// In addition to that, every manager will be registered centrally, so
+    /// every Manager can be queried from a container.
+    /// 
+    /// </summary>
+    public interface IManager
+    {
+        /// <summary>
+        /// Setting this property should be relatively lightweight, as
+        /// the majority of the (de-) initialization should happen in
+        /// <see cref="Load"/> and <see cref="Unload"/>.
+        /// </summary>
+        bool Enabled { get; set; }
+
+        /// <summary>
+        /// This property should be used to prevent falsely calling
+        /// <see cref="Load"/> or <see cref="Unload"/> multiple times
+        /// introducing bugs or leaks
+        /// </summary>
+        bool Loaded { get; }
+
+        /// <summary>
+        /// Load this manager, performing all needed heavy tasks.
+        /// Faster tasks to enable and disable the functionality,
+        /// such as some Hooks, should be done in <see cref="Enabled"/>
+        /// </summary>
+        void Load();
+
+        /// <summary>
+        /// Unloads this manager, performing all needed heavy tasks.
+        /// Faster tasks to enable and disable the functionality,
+        /// such as some Hooks, should be done in <see cref="Enabled"/>
+        /// </summary>
+        void Unload();
+    }
+}
---
+++ Lifecycle/ManagerContainer.cs
@@ -0,0 +1,95 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Andraste.Shared.Lifecycle
+{
+    #nullable enable
+    public class ManagerContainer : IManager
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        protected readonly Dictionary<Type, IManager> Managers;
+
+        public ManagerContainer()
+        {
+            Managers = new Dictionary<Type, IManager>();
+        }
+
+        public bool Enabled
+        {
+            get => true;
+            set => throw new InvalidOperationException("Cannot disable ManagerContainer");
+        }
+
+        public bool Loaded { get; private set; }
+
+        public void Load()
+        {
+            foreach (var manager in Managers.Values.Where(manager => !manager.Loaded))
+            {
+                try
+                {
+                    manager.Load();
+                    manager.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    logger.Warn(ex, "Exception when Loading or Enabling a Manager");
+                }
+            }
+
+            Loaded = true;
+        }
+
+        public void Unload()
+        {
+            foreach (var manager in Managers.Values.Where(manager => manager.Loaded))
+            {
+                try
+                {
+                    manager.Enabled = false;
+                    manager.Unload();
+                }
+                catch (Exception ex)
+                {
+                    logger.Warn(ex, "Exception when Unloading or Disabling a Manager");
+                }
+            }
+
+            Managers.Clear();
+            Loaded = false;
+        }
+
+        public void RegisterManager(IManager instance)
+        {
+            Managers.Add(instance.GetType(), instance);
+        }
+
+        public void RegisterManagerAndLoad(IManager instance)
+        {
+            RegisterManager(instance);
+            if (instance.Loaded)
+            {
+                return;
+            }
+
+            try
+            {
+                instance.Load();
+                instance.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                logger.Warn(ex, "Exception when Loading or Enabling a Manager");
+            }
+        }
+
+        public IManager? GetManager(Type type)
+        {
+            return Managers.ContainsKey(type) ? Managers[type] : null;
+        }
+    }
+    #nullable restore
+}
