c9521dc1067461f46d2b973ce078172eb3bad1b0
MeFisto94 <MeFisto94@users.noreply.github.com>
2022-06-04 14:07:54 +0200

    Json Specification: andraste.builtin.vfs.files is now a json object or c # dictionary, so that file mappings can be defined properly

--- ModManagement/Json/Features/BuiltinVfsFeature.cs
+++ ModManagement/Json/Features/BuiltinVfsFeature.cs
@@ -1,8 +1,10 @@
-﻿namespace Andraste.Shared.ModManagement.Json.Features
+﻿using System.Collections.Generic;
+
+namespace Andraste.Shared.ModManagement.Json.Features
 {
     public class BuiltinVfsFeature
     {
         public string[] Directories { get; set; }
-        public string[] Files { get; set; }
+        public Dictionary<string, string> Files { get; set; }
     }
 }
