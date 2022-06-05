abd2c99d3aba4894949cbd08025eb7590632b4c5
MeFisto94 <MeFisto94@users.noreply.github.com>
2022-06-05 04:14:21 +0200

    Move the EnabledMod class here

---
+++ ModManagement/EnabledMod.cs
@@ -0,0 +1,17 @@
+﻿using Andraste.Shared.ModManagement.Json;
+
+namespace Andraste.Shared.ModManagement
+{
+    public class EnabledMod
+    {
+        public EnabledMod(ModInformation modInformation, ModSetting modSetting)
+        {
+            ModInformation = modInformation;
+            ModSetting = modSetting;
+        }
+
+        public ModInformation ModInformation;
+        public ModSetting ModSetting;
+        public ModConfiguration ActiveConfiguration => ModInformation.Configurations[ModSetting.ActiveConfiguration];
+    }
+}
