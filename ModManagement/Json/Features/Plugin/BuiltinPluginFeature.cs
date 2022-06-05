154058f42ff5fddddc5ce09deab1fa6782d1b99c
MeFisto94 <MeFisto94@users.noreply.github.com>
2022-06-05 16:17:16 +0200

    Add the new Plugin Feature so it can be used by the Payload

---
+++ ModManagement/Json/Features/Plugin/BuiltinPluginFeature.cs
@@ -0,0 +1,8 @@
+﻿namespace Andraste.Shared.ModManagement.Json.Features.Plugin
+{
+    public class BuiltinPluginFeature
+    {
+        public string AssemblyFilePath { get; set; }
+        public string PluginClassName { get; set; }
+    }
+}
---
+++ ModManagement/Json/Features/Plugin/PluginFeatureParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Andraste.Shared.ModManagement.Json.Features.Plugin
+{
+    public class PluginFeatureParser : IFeatureParser
+    {
+        private static readonly JsonSerializerOptions ReadingOptions = new JsonSerializerOptions
+        {
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true,
+            // Writing Option: PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            PropertyNameCaseInsensitive = true
+        };
+    
+        public dynamic Parse(JsonObject obj)
+        {
+            return obj.Deserialize<BuiltinPluginFeature>(ReadingOptions);
+        }
+    }
+}
